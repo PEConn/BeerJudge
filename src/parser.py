c378ea8bed54da4cb9a95a63fa274a12842aa37d
michellexyeo <michboo@gmail.com>
2015-02-07 17:55:57 +0000

    tweaked matching algo

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,7 @@
 __author__ = 'michelleyeo'
 from flask import Flask
 import nltk
-import cPickle
+import TFIDF_thingy
 import itertools
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
@@ -13,42 +13,47 @@ def unique_list(l):
     return ulist
 
 def topn(n, list):
-    if (n < len(list)):
+    if (n > len(list)):
         return list
     else:
-        return list[:n]
+        return list[0:n]
 
 ################################
+TFIDF_thingy.init()
 
 # Get the set of English stopwords from nltk
 stop = stopwords.words('english')
+stop.extend(["sauce", "side", "served", "english", "french", "italian", "mixed", "leaves", "baked", "toasted", "fried", "cooked", "spanish", "seasonal", "w"])
 tokeniser = RegexpTokenizer(r'\w+')
 
 # Tokenise and clean string, remove duplicates
-testString = "Chicken teriyaki in peri-peri sauce, served with a side of fries"
+testString = "Fried kimchi (pickled chinese cabbage) & pork w/ steamed tofu"
+testString = testString.lower()
 tokenisedString = tokeniser.tokenize(testString)
-for word in tokenisedString:
-    word.lower()
-    if word in stop:
-        tokenisedString.remove(word)
-unique_list(tokenisedString)
+filtered_words = [w for w in tokenisedString if not w in stop]
+uniqueFilteredWords = unique_list(filtered_words)
 
 # Sort and get top n words
 n = 3
 wordList = []
-for word in tokenisedString:
-    if word in d.keys():
-        wordList.append((word, d[word]))
+for word in uniqueFilteredWords:
+    if word in TFIDF_thingy.d:
+        wordList.append((word, TFIDF_thingy.d[word]))
     else:
         wordList.append((word, 0))
-wordList.sort(key=lambda tup: tup[1])
+
+wordList.sort(key=lambda tup: tup[1], reverse=True)
 topWordList = topn(n, wordList)
 
-# Greedy search- search for all permutations of the top n words
-for i in range(n):
-    tupleList = itertools.permutations(topWordList, i)
-    for tuple in tupleList:
-        #search for thingy
+# transform into string and pass to beer API
+query = ""
+for word in topWordList:
+    query += " "
+    query += word[0]
+print(query)
+
+
+
 
 
 
