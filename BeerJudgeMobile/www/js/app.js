ead419e599d741528fc35381661b842cc82b3ba1
hnphan <hieuphan91@gmail.com>
2015-02-07 14:21:43 +0000

    added location based map screen

--- BeerJudgeMobile/www/js/app.js
+++ BeerJudgeMobile/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'ngMap', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -39,6 +39,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
+    .state('app.quickrec', {
+    url: "/quickrec",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/quickrec.html",
+        controller: 'MarkerRemoveCtrl'
+      }
+    }
+  })
+
   .state('app.browse', {
     url: "/browse",
     views: {
@@ -67,5 +77,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  // this becomes the home screen too?
+  $urlRouterProvider.otherwise('/app/quickrec');
 });
--- BeerJudgeMobile/www/js/controllers.js
+++ BeerJudgeMobile/www/js/controllers.js
@@ -45,4 +45,56 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('MarkerRemoveCtrl', function($scope, $ionicLoading) {
+
+  $scope.positions = [{
+    lat: 43.07493,
+    lng: -89.381388
+  }];
+
+  $scope.centerOnMe= function(){
+  $scope.positions = [];
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
+
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      $scope.positions.push({lat: pos.k,lng: pos.B});
+      console.log(pos);
+      $scope.map.setCenter(pos);
+      $ionicLoading.hide();
+    });
+
+  };
+
+  $scope.$on('mapInitialized', function(event, map) {
+    console.log("before map");
+    $scope.map = map;
+    $scope.positions = [];
+    console.log("after map");
+    $ionicLoading.show({
+        template: 'Loading...'
+    });
+
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      $scope.positions.push({lat: pos.k,lng: pos.B});
+      console.log(pos);
+      $scope.map.setCenter(pos);
+      $ionicLoading.hide();
+    });  
+
+  });
+
 });
+
+
+
+
+
+
