104c5cb2d76c5839f783777f08fb0192485cb5cb
Jack Fletcher <jackfletcher1@Jacks-MacBook-Pro-2.local>
2015-02-07 12:54:04 +0000

    rudimentry database

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,23 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+             abort, render_template, flash
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 @app.route("/")
 def hello():
     return "Hello World!"
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
